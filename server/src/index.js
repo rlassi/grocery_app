79825e93de9a3f309057e130a0e934e58e69ec08
Ryan Lassiter <ryankylelassiter@gmail.com>
2020-08-07 10:29:20 -0400

    update scripts for production env; update server/index.js to use react-app static build files

--- server/src/index.js
+++ server/src/index.js
@@ -15,6 +15,12 @@ app.use(express.json());
 app.use(userRouter);
 app.use(productRouter);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html' ))
+    })
+}
 app.listen(port, () => {
     console.log(`server is up on port ${port}`)
 });
