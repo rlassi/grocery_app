e9ea20065b31113370fcadb9a1ec7be01d24956b
Ryan Lassiter <ryankylelassiter@gmail.com>
2020-08-08 21:19:41 -0400

    add conditional NODE_ENV check for database connection URL

--- server/src/db/mongoose.js
+++ server/src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_CONNECTION, (err, db) => {
+const devURI = 'mongodb://127.0.0.1:27017'
+mongoose.connect(process.env.NODE_ENV === 'production' ? process.env.DB_CONNECTION : devURI, (err, db) => {
     if(err){
         console.log('unable to connect to mongodb')
     } else {
